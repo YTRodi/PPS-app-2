0dca558fb1a3a0c9afca0d8fa910fa93126d8459
YTRodi <yagorodi@gmail.com>
2022-04-15 10:57:36 -0300

    feat(build-config): eas.json

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,40 @@
 import { ScrollView, View, StyleSheet } from 'react-native';
-import { Title, Paragraph } from 'react-native-paper';
+import { Title, Paragraph, Button } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useToast } from 'react-native-toast-notifications';
+import { FullPageSpinner } from '../../components';
+import { useAsync } from '../../hooks';
 import { useAuth } from '../../context/AuthProvider';
+import { mapAuthError } from '../../helpers';
 
 function Home() {
   const { bottom } = useSafeAreaInsets();
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const { isLoading, isError, error, run } = useAsync();
+  const toast = useToast();
+
+  if (isLoading) {
+    return <FullPageSpinner />;
+  }
+
+  if (isError) {
+    const errorMessage = mapAuthError(error);
+    toast.show(errorMessage, { type: 'danger' });
+  }
 
   return (
     <ScrollView style={{ paddingBottom: bottom }}>
       <View style={styles.container}>
         <Title>Información del usuario:</Title>
         <Paragraph>{JSON.stringify(user, null, 2)}</Paragraph>
+        <Button
+          mode='contained'
+          onPress={() => run(logout())}
+          disabled={isLoading}
+          loading={isLoading}
+        >
+          Salir
+        </Button>
       </View>
     </ScrollView>
   );
@@ -19,7 +42,7 @@ function Home() {
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 24,
+    margin: 24,
   },
 });
 
